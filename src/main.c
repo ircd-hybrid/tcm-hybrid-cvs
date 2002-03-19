f01e6768a85dd693303a18f2458e511cb667a93c
wcampbel <wcampbel>
2002-03-19 23:54:34 +0000

    Ignore SIGTRAP to prevent tcm from coring when detaching gdb on FreeBSD

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 *   Based heavily on Adam Roach's bot skeleton.             *
 ************************************************************/
 
-/* $Id: main.c,v 1.26 2002/03/06 10:58:57 einride Exp $ */
+/* $Id: main.c,v 1.27 2002/03/19 23:54:34 wcampbel Exp $ */
 
 #include "setup.h"
 
@@ -637,6 +637,7 @@ int main(int argc, char *argv[])
   signal(SIGINT,sighandlr);
   signal(SIGHUP,reload_user_list);
   signal(SIGPIPE, SIG_IGN);
+  signal(SIGTRAP, SIG_IGN);
   /* pick up the name of a pid file from the tcm.cf file */
 #ifdef DEBUGMODE
   config_entries.debug=1;

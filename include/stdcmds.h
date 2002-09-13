96d96ef6222b0f26015e43efa5215227edf0cfcf
bill <bill>
2002-09-13 03:38:18 +0000

    - ergh, forgot to fix prototype

--- include/stdcmds.h
+++ include/stdcmds.h
@@ -1,7 +1,7 @@
 #ifndef __STDCMDS_H
 #define __STDCMDS_H
 
-/* $Id: stdcmds.h,v 1.33 2002/08/08 18:10:35 bill Exp $ */
+/* $Id: stdcmds.h,v 1.34 2002/09/13 03:38:18 bill Exp $ */
 
 void op(char *chan,char *nick);
 void kick(char* chan,char* nick,char *comment);
@@ -12,7 +12,7 @@ void print_motd(struct connection *connection_p);
 void report(int type, char *format,...);
 
 void do_a_kline(int kline_time, char *pattern, char *reason,
-                char *who_did_command);
+                struct connection *connection_p);
 
 void init_hash(void);
 void oper();
